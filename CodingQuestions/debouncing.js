cbd81c4320b0431d9de6454141408e8c952d3383
kartikey1112 <kartikeybajpai2811@gmail.com>
2025-01-05 20:15:29 +0530

    added debouncing

---
+++ CodingQuestions/debouncing.js
@@ -0,0 +1,12 @@
+function debounce(func, wait) {
+    let debounceTimer
+              return function () {
+                  const context = this  
+                  const args = arguments
+                  clearTimeout(debounceTimer)
+                  debounceTimer
+                      = setTimeout(() => func.apply(context, args), wait)
+              }
+  }
+
+
